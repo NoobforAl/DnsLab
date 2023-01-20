4d82370f085e750b3dd18c1d0066e4c7c56fb1ec
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-01-20 19:50:01 +0330

    add model ipOrHostPing
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- core/model.go
+++ core/model.go
@@ -1,6 +1,18 @@
 package core
 
-type response struct {
+type ipInfo struct {
 	IPv4 string `json:"iPv4"`
 	IPv6 string `json:"iPv6"`
+}
+
+type ipOrHostPing struct {
+	BufferSize int    `json:"bufferSize"`
+	Ip         string `json:"ip"`
+	Ttl        int    `json:"ttl"`
+	Time       int    `json:"time"`
+	Success    string `json:"success"`
+}
+
+type response interface {
+	ipOrHostPing | ipInfo
 }
