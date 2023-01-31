ba942826c438d1f0443c889e770a82a80a71b6f3
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-01-31 23:27:03 +0330

    add dnsLook models
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- core/model.go
+++ core/model.go
@@ -13,6 +13,59 @@ type ipOrHostPing struct {
 	Success    bool   `json:"success"`
 }
 
+type dnsLookUpQT1 struct {
+	RecordType string `json:"recordType"`
+	DomainName string `json:"domainName"`
+	Ttl        int    `json:"ttl"`
+	Address    string `json:"address"`
+}
+
+type dnsLookUpQT2 struct {
+	RecordType string `json:"recordType"`
+	DomainName string `json:"domainName"`
+	Ttl        int    `json:"ttl"`
+	NsdName    string `json:"nsdName"`
+}
+
+type dnsLookUpQT5 struct {
+	RecordType    string `json:"recordType"`
+	DomainName    string `json:"domainName"`
+	Ttl           int    `json:"ttl"`
+	CanonicalName string `json:"canonicalName"`
+}
+
+type dnsLookUpQT6 struct {
+	RecordType string `json:"recordType"`
+	DomainName string `json:"domainName"`
+	Ttl        int    `json:"ttl"`
+	Expire     int    `json:"expire"`
+	Minimum    int    `json:"minimum"`
+	MName      string `json:"mName"`
+	Refresh    int    `json:"refresh"`
+	Retry      int    `json:"retry"`
+	RName      string `json:"rName"`
+	Serial     int    `json:"serial"`
+}
+
+type dnsLookUpQT16 struct {
+	RecordType string `json:"recordType"`
+	DomainName string `json:"domainName"`
+	Ttl        int    `json:"ttl"`
+	Exchange   string `json:"exchange"`
+	Preference int    `json:"preference"`
+}
+
+type qTypes struct {
+	Q1  []dnsLookUpQT1
+	Q2  []dnsLookUpQT2
+	Q5  []dnsLookUpQT5
+	Q6  []dnsLookUpQT6
+	Q16 []dnsLookUpQT16
+}
+
 type response interface {
-	ipOrHostPing | ipInfo
+	ipOrHostPing | ipInfo |
+		[]dnsLookUpQT1 | []dnsLookUpQT2 |
+		[]dnsLookUpQT5 | []dnsLookUpQT6 |
+		[]dnsLookUpQT16
 }
