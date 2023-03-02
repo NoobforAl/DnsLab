be07e53fa74e5a9c1546af5109d59424dd635224
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-03-03 01:21:52 +0330

    remove some models and set comment for each model
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- core/model.go
+++ core/model.go
@@ -1,10 +1,18 @@
 package core
 
+/*
+* use for checkIP() method for check user ip
+* and setup value response to config struct
+ */
 type ipInfo struct {
 	IPv4 string `json:"iPv4"`
 	IPv6 string `json:"iPv6"`
 }
 
+/*
+* use for ping your ip or other ip
+* ping() method use this
+ */
 type ipOrHostPing struct {
 	BufferSize int    `json:"bufferSize"`
 	Ip         string `json:"ip"`
@@ -13,59 +21,67 @@ type ipOrHostPing struct {
 	Success    bool   `json:"success"`
 }
 
-type dnsLookUpQT1 struct {
+/*
+* this interface use for decoder json
+* and some returned value then we don't know type
+ */
+type response interface {
+	ipOrHostPing | ipInfo
+}
+
+/*
+! wrong: all query type in program not used!
+! this for see what's response
+* this struct is base info for all query type
+* then we request
+* and model for all query type struct
+* ( DnsLookUpQT1, DnsLookUpQT2,
+*	DnsLookUpQT5, DnsLookUpQT6, DnsLookUpQT16)
+*/
+type dnsLookUpQT struct {
 	RecordType string `json:"recordType"`
 	DomainName string `json:"domainName"`
 	Ttl        int    `json:"ttl"`
-	Address    string `json:"address"`
 }
 
-type dnsLookUpQT2 struct {
-	RecordType string `json:"recordType"`
-	DomainName string `json:"domainName"`
-	Ttl        int    `json:"ttl"`
-	NsdName    string `json:"nsdName"`
+// * model for query type 1
+type DnsLookUpQT1 struct {
+	dnsLookUpQT
+
+	Address string `json:"address"`
 }
 
-type dnsLookUpQT5 struct {
-	RecordType    string `json:"recordType"`
-	DomainName    string `json:"domainName"`
-	Ttl           int    `json:"ttl"`
+// * model for query type 2
+type DnsLookUpQT2 struct {
+	dnsLookUpQT
+
+	NsdName string `json:"nsdName"`
+}
+
+// * model for query type 5
+type DnsLookUpQT5 struct {
+	dnsLookUpQT
+
 	CanonicalName string `json:"canonicalName"`
 }
 
-type dnsLookUpQT6 struct {
-	RecordType string `json:"recordType"`
-	DomainName string `json:"domainName"`
-	Ttl        int    `json:"ttl"`
-	Expire     int    `json:"expire"`
-	Minimum    int    `json:"minimum"`
-	MName      string `json:"mName"`
-	Refresh    int    `json:"refresh"`
-	Retry      int    `json:"retry"`
-	RName      string `json:"rName"`
-	Serial     int    `json:"serial"`
+// * model for query type 6
+type DnsLookUpQT6 struct {
+	dnsLookUpQT
+
+	Expire  int    `json:"expire"`
+	Minimum int    `json:"minimum"`
+	MName   string `json:"mName"`
+	Refresh int    `json:"refresh"`
+	Retry   int    `json:"retry"`
+	RName   string `json:"rName"`
+	Serial  int    `json:"serial"`
 }
 
-type dnsLookUpQT16 struct {
-	RecordType string `json:"recordType"`
-	DomainName string `json:"domainName"`
-	Ttl        int    `json:"ttl"`
+// * model for query type 16
+type DnsLookUpQT16 struct {
+	dnsLookUpQT
+
 	Exchange   string `json:"exchange"`
 	Preference int    `json:"preference"`
-}
-
-type qTypes struct {
-	Q1  []dnsLookUpQT1
-	Q2  []dnsLookUpQT2
-	Q5  []dnsLookUpQT5
-	Q6  []dnsLookUpQT6
-	Q16 []dnsLookUpQT16
-}
-
-type response interface {
-	ipOrHostPing | ipInfo |
-		[]dnsLookUpQT1 | []dnsLookUpQT2 |
-		[]dnsLookUpQT5 | []dnsLookUpQT6 |
-		[]dnsLookUpQT16
 }
