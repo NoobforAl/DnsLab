9211d921771e3efb77e8671765d184192e865a1a
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-03-03 01:26:14 +0330

    fix main file for run command & control key input( ctrl + c)
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,127 +1,34 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
-	"reflect"
-	"time"
+	"os/signal"
+	"syscall"
 
-	"github.com/NoobforAl/DnsLab/core"
+	"github.com/NoobforAl/DnsLab/cmd"
 	log "github.com/sirupsen/logrus"
 )
 
-var app core.BaseConf = core.BaseConf{}
-
-func init() {
-	if len(os.Args) >= 1 {
-		if err := app.CheckIP(); err != nil {
-			log.Fatal(err.Error())
-		}
+func main() {
+	if err := recover(); err != nil {
+		log.Warn("Background Task Exit (handel keyPut exit!)", err)
 	}
+
+	go handelKeyPut()
+	os.Exit(cmd.Run())
 }
 
-func main() {
-	showIP := flag.Bool("ip", false, "See Your Ip")
-	token := flag.String("t", "", "Set your token")
-	pingIP := flag.Bool("pi", false, "Ping your IP")
-	openPort := flag.Bool("op", false, "Open Port Checker")
-	dnsLookup := flag.Bool("dl", false, "Dns Lookup")
-	reverseLookup := flag.Bool("rl", false, "Reverse Lookup")
-	updateIP := flag.Bool("uip", false, "Update your ip with token!")
-	upApp := flag.Bool("up", false, "every 3h or any time check your ip!")
-	timeUpdate := flag.Int("ts", 3, "time sleep for check ip")
-	flag.Parse()
+/*
+* Handel ctrl + c keyPut
+ */
+func handelKeyPut() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGALRM)
 
-	app.Token = *token
+	// * wait for get data from chanel
+	<-c
 
-	if len(os.Args) <= 1 {
-		flag.PrintDefaults()
-		return
-	}
-
-	if app.Token != "" {
-		v, err := app.UpdateIp()
-		app.Show(err, fmt.Sprintf("IP updated: %v", v))
-	}
-
-	if *pingIP {
-		v, err := app.Ping()
-		app.Show(err,
-			fmt.Sprintf("BufferSize: %d", v.BufferSize),
-			fmt.Sprintf("Ip: %s", v.Ip),
-			fmt.Sprintf("Ttl: %d", v.Ttl),
-			fmt.Sprintf("Time: %d", v.Time),
-			fmt.Sprintf("Success: %v", v.Success),
-		)
-	}
-
-	if *openPort {
-		v, err := app.OpenPort()
-		app.Show(err, fmt.Sprintf("Port is open: %v", v))
-	}
-
-	if *dnsLookup {
-		data, err := app.DnsLookup()
-
-		if len(data.Q1) != 0 {
-			for _, v := range data.Q1 {
-				v := reflect.ValueOf(v)
-				for i := 0; i < v.NumField(); i++ {
-					app.Show(err, fmt.Sprintf("%s: %s", v.Type().Field(i).Name, v.Field(i)))
-				}
-			}
-		} else if len(data.Q2) != 0 {
-			for _, v := range data.Q2 {
-				v := reflect.ValueOf(v)
-				for i := 0; i < v.NumField(); i++ {
-					app.Show(err, fmt.Sprintf("%s: %s", v.Type().Field(i).Name, v.Field(i)))
-				}
-			}
-		} else if len(data.Q5) != 0 {
-			for _, v := range data.Q5 {
-				v := reflect.ValueOf(v)
-				for i := 0; i < v.NumField(); i++ {
-					app.Show(err, fmt.Sprintf("%s: %s", v.Type().Field(i).Name, v.Field(i)))
-				}
-			}
-		} else if len(data.Q6) != 0 {
-			for _, v := range data.Q6 {
-				v := reflect.ValueOf(v)
-				for i := 0; i < v.NumField(); i++ {
-					app.Show(err, fmt.Sprintf("%s: %s", v.Type().Field(i).Name, v.Field(i)))
-				}
-			}
-		} else if len(data.Q16) != 0 {
-			for _, v := range data.Q16 {
-				v := reflect.ValueOf(v)
-				for i := 0; i < v.NumField(); i++ {
-					app.Show(err, fmt.Sprintf("%s: %s", v.Type().Field(i).Name, v.Field(i)))
-				}
-			}
-		}
-	}
-
-	if *reverseLookup {
-		v, err := app.ReverseLookup()
-		app.Show(err, fmt.Sprintf("DNS: %s", v))
-	}
-
-	if *updateIP {
-		v, err := app.UpdateIp()
-		app.Show(err, fmt.Sprintf("IP updated: %v", v))
-	}
-
-	if *showIP {
-		log.Println("Ipv4: ", app.Ip)
-		log.Println("Ipv6: ", app.Ipv6)
-	}
-
-	if *upApp {
-		for {
-			v, err := app.UpdateIp()
-			app.Show(err, fmt.Sprintf("IP updated: %v", v))
-			time.Sleep(time.Duration(*timeUpdate) * time.Hour)
-		}
-	}
+	log.Println("Exit Program....")
+	log.Println("Good bye!")
+	os.Exit(0)
 }
