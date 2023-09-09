1dcc068ca57ae668e81998c55842f61271298ba4
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-09-09 15:55:22 +0330

    fix core system, better function & config
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/core/core.go
+++ src/core/core.go
@@ -1,90 +1,117 @@
 package core
 
 import (
+	"errors"
 	"fmt"
-	"log"
-	"net/http"
+	"strings"
+	"time"
+
+	"github.com/NoobforAl/DnsLab/src/contract"
 )
 
 /*
 * basic config for request to api
  */
 type BaseConf struct {
-	Token string
-	Ip    string
-	Ipv6  string
-	Port  uint16
+	retryTime  time.Duration
+	retryCount uint
+
+	log   contract.Logger
+	token string
+}
+
+func New(
+	token string,
+	log contract.Logger,
+	retryTime time.Duration,
+	retryCount uint,
+) *BaseConf {
+	return &BaseConf{
+		token:      token,
+		log:        log,
+		retryTime:  retryTime,
+		retryCount: retryCount,
+	}
 }
 
 /*
-* check system ip with request to api.dnslab.ir
+* check system ip with request to api.dnslab.link
 * and set ip4 and ip6 and port for config
  */
-func (c *BaseConf) CheckIP() error {
-	res, err := request(IP)
+func (c *BaseConf) CheckIP() (map[string]any, error) {
+	res, err := c.request(IP)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	var data ipInfo
-	if err := decodeBodyJson(res, &data); err != nil {
-		return err
+	data := make(map[string]any)
+	if err := c.decodeBodyJson(res, &data); err != nil {
+		return nil, err
 	}
 
-	c.SetUpIp(data.IPv4, data.IPv6, 0)
-	return nil
+	return data, nil
 }
 
 /*
 * ping your ip for check get response or not
  */
-func (c *BaseConf) Ping() (*ipOrHostPing, error) {
-	url := fmt.Sprintf(PING, c.Ip)
-	res, err := request(url)
+func (c *BaseConf) Ping(ipOrHost string) (map[string]any, error) {
+	url := fmt.Sprintf(PING, ipOrHost)
+	res, err := c.request(url)
 	if err != nil {
 		return nil, err
 	}
 
-	var data ipOrHostPing
-	if err := decodeBodyJson(res, &data); err != nil {
+	data := make(map[string]any)
+	if err := c.decodeBodyJson(res, &data); err != nil {
 		return nil, err
 	}
 
-	return &data, nil
+	return data, nil
 }
 
 /*
 * check one port form on ip is open or not
  */
-func (c *BaseConf) OpenPort() (bool, error) {
-	url := fmt.Sprintf(OPEN, c.Ip, c.Port)
-	res, err := request(url)
+func (c BaseConf) OpenPort(ipOrHost string, prot uint16) (bool, error) {
+	url := fmt.Sprintf(OPEN, ipOrHost, prot)
+	res, err := c.request(url)
 	if err != nil {
-		res.Body.Close()
 		return false, err
 	}
-	return boolPars(res)
+	s, err := c.bodyToString(res)
+	return strings.EqualFold(s, "true"), err
 }
 
 /*
 * get response with out pars data with
 * response models type query!
  */
-func (c *BaseConf) DnsLookup(q string) (*http.Response, error) {
-	url := fmt.Sprintf(DNSLOOKUP, c.Ip, q)
-	return request(url)
+func (c BaseConf) DnsLookup(ipOrHost, q string) (map[string]any, error) {
+	url := fmt.Sprintf(DNS_LOOKUP, ipOrHost, q)
+	res, err := c.request(url)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make(map[string]any)
+	if err := c.decodeBodyJson(res, &data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
 
 /*
 * Reverse lookup ip/host and return string
  */
-func (c *BaseConf) ReverseLookup() (string, error) {
-	url := fmt.Sprintf(REVERSELookup, c.Ip)
-	res, err := request(url)
+func (c BaseConf) ReverseLookup(ip string) (string, error) {
+	url := fmt.Sprintf(REVERSELookup, ip)
+	res, err := c.request(url)
 	if err != nil {
 		return "", err
 	}
-	return bodyToString(res)
+	return c.bodyToString(res)
 }
 
 /*
@@ -93,35 +120,15 @@ func (c *BaseConf) ReverseLookup() (string, error) {
 * and run with timing
 * for example every 1 hour run this
  */
-func (c *BaseConf) UpdateIp() (bool, error) {
-	url := fmt.Sprintf(DNSREF, c.Token)
-	res, err := request(url)
+func (c BaseConf) UpdateIp() (bool, error) {
+	url := fmt.Sprintf(DNS_REF, c.token)
+	res, err := c.request(url)
 	if err != nil {
 		return false, err
 	} else if res.StatusCode != 200 {
-		return false, fmt.Errorf("token not found")
+		txt, _ := c.bodyToString(res)
+		err = fmt.Errorf("body-> %s", txt)
+		return false, errors.Join(AUTH_ERR, err)
 	}
 	return true, nil
-}
-
-/*
-* setup new ip/port for config
-* if IPv4 is empty call method CheckIP()
-* if port equal 0 set port 80
- */
-func (c *BaseConf) SetUpIp(IPv4, IPv6 string, port uint16) {
-	if IPv4 == "" {
-		if err := c.CheckIP(); err != nil {
-			log.Panic(err)
-		}
-		return
-	}
-
-	c.Ip = IPv4
-	c.Ipv6 = IPv6
-
-	c.Port = port
-	if port == 0 {
-		c.Port = 80
-	}
 }
