fa7ca12cd01761750819b8591ed69ba162bcb7c5
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-09-09 17:13:33 +0330

    combine 3 files
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/NoobforAl/DnsLab/src/contract"
@@ -12,6 +14,27 @@ import (
 )
 
 var (
+	pingIP = flag.Bool("pi", false, "Ping your IP")
+	showIP = flag.Bool("show-ip", false, "See Your Ip")
+
+	debug = flag.Bool("d", false, "run debug mode")
+
+	openPort      = flag.Bool("op", false, "Open Port Checker")
+	reverseLookup = flag.Bool("rl", false, "Reverse Lookup")
+
+	upApp    = flag.Bool("check-ip", false, "every 3m or any time check your ip!")
+	updateIP = flag.Bool("update-ip", false, "Update your ip with token! use this command with -t <your token>")
+
+	ipOrHost  = flag.String("addr", "", "Set your ip or host")
+	dnsLookup = flag.String("dl", "", "Dns Lookup ues -dl query type")
+	token     = flag.String("token", "", "Set your token use -t your token")
+
+	timeUpdate = flag.Duration("time-check", time.Minute*3, "time sleep for check ip, default is 3m")
+	retryTime  = flag.Duration("retry-time", time.Second*3, "time sleep for request again if get error, default is 3s")
+
+	port       = flag.Uint("port", 80, "set your port for request")
+	retryCount = flag.Uint("retry-count", 3, "how many time try for request again, default is 3  times")
+
 	app contract.DnslabAPi
 	log contract.Logger
 )
@@ -112,5 +135,62 @@ func main() {
 			printData(fmt.Sprintf("IP updated: %v", v))
 			<-time.After(*timeUpdate)
 		}
+	}
+}
+
+func printData(m any) {
+	switch m := m.(type) {
+	case string:
+		log.Infof("%s", m)
+
+	case bool:
+		log.Infof("%b", m)
+
+	case map[string]any:
+		for k, v := range m {
+			log.Infof("%s: %v", k, v)
+		}
+
+	case []map[string]any:
+		for _, v := range m {
+			log.Info("------------------")
+			printData(v)
+			log.Info("------------------")
+		}
+	}
+}
+
+// * check enter query type is valid
+func checkQueryType(q *string) error {
+	switch *q {
+	case "1", "2", "5", "6", "16":
+		return nil
+	default:
+		return fmt.Errorf("not found query type")
+	}
+}
+
+/*
+* Handel ctrl + c keyPut
+ */
+func handelKeyInput() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGALRM)
+
+	// * wait for get data from chanel
+	<-c
+
+	log.Println("Exit Program....")
+	log.Println("Good bye!")
+	os.Exit(0)
+}
+
+/*
+* recover panic code
+ */
+func recoverPanic() {
+	if err := recover(); err != nil {
+		log.Warn(err)
+		os.Exit(1)
 	}
 }
